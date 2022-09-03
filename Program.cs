dea8c9bdd10b752d30e536659315bbe678d63d9a
cristianbravoq <cristian.bravoq@hotmail.com>
2022-09-02 21:34:52 -0500

    Open API fue implementado

--- Program.cs
+++ Program.cs
@@ -3,9 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -13,7 +13,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI( c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json","Prueba API V1");
+    });
 }
 
 app.UseHttpsRedirection();
